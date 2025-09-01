3492a85623a2a220a2457df2aa4a36d6e29d27fc
Łukasz Świszcz <luk.swiszcz@gmail.com>
2025-09-01 18:25:34 +0200

    Scaffold Quartus HDL project

---
+++ hdl/modules/nrzi_phase_lock_decoder.sv
@@ -0,0 +1,9 @@
+module nrzi_phase_lock_decoder (
+    input logic     clk_i,
+    input logic     clk_x4_i,
+    input logic     nrzi_i,
+    output logic    data_o,
+    output logic    valid_o
+);
+    // TODO: write logic
+endmodule
